c704b3ca2364d3befc0480e96a2bbc8a5046d288
Rairton <rairtoncsc@gmail.com>
2026-01-11 21:21:49 -0300

    Update API URL for production environment

--- Assets/config.js
+++ Assets/config.js
@@ -1,6 +1,7 @@
 // Altere a URL conforme o ambiente
 const API_URL = window.location.hostname === 'localhost'
   ? 'http://localhost:3001/api'
-  : 'https://seu-dominio.com/api';
+  : 'https://model-barber-backend.onrender.com/api';
 
 export default API_URL;
+
